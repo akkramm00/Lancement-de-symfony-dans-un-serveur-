86552432ea57282f54fc1a3b397d37660442e057
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-15 20:12:37 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+<h1>Lancement de Symfony</h1>
+  Après avor installer Symfony, NOus avopns compris de quoi il etait fait, maintenant nous alons voir sans doute à quoi il ressemble. Pour cela , nous allons lancer un serveur PHP afin de pouvoir interpréter le code php et afficher nos pages dans le navigateur.
+    
+  </body>
+</html>
